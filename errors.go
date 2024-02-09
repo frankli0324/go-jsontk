b2f0509d7053cd7e430ea868292ac81bbfbce520
Frank <lixuanzhe@bytedance.com>
2024-02-10 00:11:45 +0800

    optimize validation

--- errors.go
+++ errors.go
@@ -5,6 +5,7 @@ import "errors"
 var (
 	ErrPanic              = errors.New("panic occurred")
 	ErrUnexpectedSep      = errors.New("invalid separator")
+	ErrEarlyEOF           = errors.New("early EOF")
 	ErrUnexpectedToken    = errors.New("invalid TokenType")
 	ErrInvalidParentheses = errors.New("invalid parentheses")
 )
