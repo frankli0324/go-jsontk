47e1823adb2ed19eba4438952758081311ffebc6
Frank <frankli0324@hotmail.com>
2024-12-31 13:01:48 +0800

    feat: remove precheck in unquote for inline

--- unquote_1.18.go
+++ unquote_1.18.go
@@ -3,19 +3,11 @@
 package jsontk
 
 import (
-	"bytes"
 	"reflect"
 	"unsafe"
 )
 
 func unquote(s []byte) (t string, ok bool) {
-	if len(s) < 2 || s[0] != '"' || s[len(s)-1] != '"' {
-		return
-	}
-	if bytes.IndexByte(s, '\\') < 0 {
-		s = s[1 : len(s)-1]
-		return *(*string)(unsafe.Pointer(&s)), true
-	}
 	s, ok = unquoteBytes(s)
 	t = *(*string)(unsafe.Pointer(&s))
 	return
--- unquote_1.20.go
+++ unquote_1.20.go
@@ -3,17 +3,10 @@
 package jsontk
 
 import (
-	"bytes"
 	"unsafe"
 )
 
 func unquote(s []byte) (t string, ok bool) {
-	if len(s) < 2 || s[0] != '"' || s[len(s)-1] != '"' {
-		return
-	}
-	if bytes.IndexByte(s, '\\') < 0 {
-		return unsafe.String(unsafe.SliceData(s[1:len(s)-1]), len(s)-2), true
-	}
 	s, ok = unquoteBytes(s)
 	t = unsafe.String(unsafe.SliceData(s), len(s))
 	return
