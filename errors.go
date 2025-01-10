868c4c1502cd6c2a75de22f86ee648e50b9192f3
Frank <frankli0324@hotmail.com>
2025-01-10 19:57:54 +0800

    fix: uncommitted error

--- errors.go
+++ errors.go
@@ -6,6 +6,7 @@ var (
 	ErrPanic              = errors.New("panic occurred")
 	ErrUnexpectedSep      = errors.New("invalid separator")
 	ErrEarlyEOF           = errors.New("early EOF")
+	ErrInterrupt          = errors.New("interrupted by user")
 	ErrUnexpectedToken    = errors.New("invalid TokenType")
 	ErrInvalidParentheses = errors.New("invalid parentheses")
 	ErrStandardViolation  = errors.New("json not compliant to RFC8259") // for some simple validations
